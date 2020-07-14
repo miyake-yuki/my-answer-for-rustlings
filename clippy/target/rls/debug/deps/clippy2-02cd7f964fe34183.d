64df6c8d66fb2e9c3aa52e7d7e00adb3c04fd23d
santamn <cle.neige@gmail.com>
2020-07-14 16:46:20 +0900

    solved 20200714

---
+++ clippy/target/rls/debug/deps/clippy2-02cd7f964fe34183.d
@@ -0,0 +1,5 @@
+/Users/miyakeyuuki/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-02cd7f964fe34183.rmeta: clippy2.rs
+
+/Users/miyakeyuuki/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-02cd7f964fe34183.d: clippy2.rs
+
+clippy2.rs:
---
+++ clippy/target/rls/debug/deps/clippy2-8db7ad8a71c0084f.d
@@ -0,0 +1,5 @@
+/Users/miyakeyuuki/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-8db7ad8a71c0084f.rmeta: clippy2.rs
+
+/Users/miyakeyuuki/rustlings/exercises/clippy/target/rls/debug/deps/clippy2-8db7ad8a71c0084f.d: clippy2.rs
+
+clippy2.rs:
